05d4472eeddb1085a64cfaae526137f4f9b116e0
idanguyen <115324491+idanguyen@users.noreply.github.com>
2024-01-16 21:02:23 +0100

    add clarify user role and string utilities
    Added sting compare function and function to clarify user role

--- src/js/utilities/index.js
+++ src/js/utilities/index.js
@@ -1,3 +1,4 @@
 export * from './search/searchFunctionTemplate.js';
 export * from './search/getSearchTerms.js';
 export * from './search/searchNotFoundDisplay.js';
+export * from './strings/strings.js';
